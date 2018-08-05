7c12b455ec828decc3f5b0c4c75ea8f7250609b8
davidseo <jseo49ut@gmail.com>
2018-08-05 16:42:23 -0400

    4 problems of exercise2

---
+++ exercise2.rb
@@ -0,0 +1,24 @@
+# Calculating a good tip for a 55 dollar meal
+
+meal_price = 55
+tip = meal_price * 0.15
+puts "A good tip for a 55 dollar meal would be #{tip}"
+
+
+# Adding a string and an integer
+
+word = "Three"
+number = 3
+puts word + number.to_s
+
+
+# string interpolation
+
+x = 45628 * 7839
+puts "We find the value of x to be #{x}"
+
+# the value of (true && false) || (false && true) || !(false && false)...
+# false || false || true
+# false || true
+# true
+puts (true && false) || (false && true) || !(false && false)
